82dd8624e54462c6d625c44e5fb4f98b5b6b1409
AdityaDubey422 <adityadubey5369@gmail.com>
2024-11-14 07:34:37 +0000

    learned data tyoes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "aditya@gmaul.com"
+var accountName = "Shubh"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 1 //not allowed
+
+accountEmail = "hc@hc.com"
+accountName = "hello"
+accountName = "Bengaluru"
+
+console.table([accountId, accountEmail, accountName, accountCity,accountState])
+
+/* Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+let name = "Shubh";
+let age = 18;
+let isLoggedIn = true;
+let state;
+
+// number => 2^53
+// bigint 
+// string => "" or ''
+// boolean => true/false
+// null=> standalone value
+// undefined => abhi value assign nahi hui hai
+// symbol => unique
+
+// object 
+
+console.table([typeof name,typeof age,typeof true,typeof "hello",typeof 22,typeof isLoggedIn,typeof null,typeof state,typeof undefined]);
+console.log(typeof null); //object
+let number = BigInt(2**70)
+console.log(typeof number)
+console.log(number)
